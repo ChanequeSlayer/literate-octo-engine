55b65adb7ba76adf8332bceead8dc9a48a7dac0e
VictorGarduno <ve15421@innovaccion.mx>
2021-04-30 14:22:58 -0600

    version 1.0

---
+++ src/andres/Andres.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package andres;
+
+import java.util.Scanner;
+
+public class Andres {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner write = new Scanner(System.in);
+        String saludo = write.next();
+        int numI = write.nextInt();
+        int num1 = write.nextInt();
+        int num2 = write.nextInt();
+        int num3 = write.nextInt();
+    }
+    
+}
